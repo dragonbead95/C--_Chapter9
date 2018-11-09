d5ceb2c4b066d31892e6af086af298a23879766d
201604138 <201604138@o.cnu.ac.kr>
2018-11-09 11:29:22 +0900

    객체 안에 정말로 멤버함수가 존재하는가 2

---
+++ Chapter9_04_MultiInher1_383p/MultiInher1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class BaseOne
+{
+public:
+	void SimpleFuncOne()
+	{
+		cout << "BaseOne" << endl;
+	}
+};
+
+class BaseTwo
+{
+public:
+	void SimpleFuncTwo()
+	{
+		cout << "BaseTwo" << endl;
+	}
+};
+
+class MultiDerived : public BaseOne, public BaseTwo
+{
+public:
+	void ComplexFunc()
+	{
+		SimpleFuncOne();
+		SimpleFuncTwo();
+	}
+};
+
+int main(void)
+{
+	MultiDerived mdr;
+	mdr.ComplexFunc();
+	
+	system("pause");
+	return 0;
+}
